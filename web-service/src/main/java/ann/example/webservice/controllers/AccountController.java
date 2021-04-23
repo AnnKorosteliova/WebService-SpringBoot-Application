d6e0e50fa906d75dcd8b91f88d23625c10daf2b6
Ann Korosteliova <anna_chi_03@mail.ru>
2021-04-23 23:42:30 +0300

    added AccountController

--- web-service/src/main/java/ann/example/webservice/controllers/AccountController.java
+++ web-service/src/main/java/ann/example/webservice/controllers/AccountController.java
@@ -1,8 +1,31 @@
 package ann.example.webservice.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ann.example.webservice.domain.Account;
+import ann.example.webservice.services.AccountService;
+
 @RestController
+@RequestMapping("/account")
 public class AccountController {
 
+	@Autowired
+	AccountService accountService;
+	
+	@GetMapping("/")
+	public List<Account> index(){
+		return accountService.getAccountsList();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Account> show(@PathVariable int id) {
+		return accountService.getAccountById(id);
+	}
 }
