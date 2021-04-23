f8f4a5161b907b6fd8073e40ce265f90502623d3
Ann Korosteliova <anna_chi_03@mail.ru>
2021-04-23 23:43:18 +0300

    added AccountService

--- web-service/src/main/java/ann/example/webservice/services/AccountService.java
+++ web-service/src/main/java/ann/example/webservice/services/AccountService.java
@@ -1,8 +1,26 @@
 package ann.example.webservice.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ann.example.webservice.dao.AccountRepo;
+import ann.example.webservice.domain.Account;
 
 @Service
 public class AccountService {
 
+	@Autowired
+	AccountRepo accountRepo;
+
+	public List<Account> getAccountsList() {
+		return accountRepo.findAll();
+	}
+
+	public Optional<Account> getAccountById(int id) {
+		return accountRepo.findById(id);
+	}
+	
 }
