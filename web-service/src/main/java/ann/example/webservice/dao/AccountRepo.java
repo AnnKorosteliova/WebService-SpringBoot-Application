5fa08f1342c5be5d8e411f4738d0094fd224b509
Ann Korosteliova <anna_chi_03@mail.ru>
2021-04-27 15:55:36 +0300

    added new method

--- web-service/src/main/java/ann/example/webservice/dao/AccountRepo.java
+++ web-service/src/main/java/ann/example/webservice/dao/AccountRepo.java
@@ -1,5 +1,7 @@
 package ann.example.webservice.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,6 @@ import ann.example.webservice.domain.Currency;
 public interface AccountRepo extends JpaRepository<Account, Integer>{
 	
 	public Account findByNumber(String number);
-	public Account findByCurrency(Currency currency);
+	public List<Account> findByCurrency(Currency currency);
+	public Account findById(int id);
 }
