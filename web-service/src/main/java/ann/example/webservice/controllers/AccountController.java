02abd4c29a83b4d65e6dca33c1a7706c14a05e71
Ann Korosteliova <anna_chi_03@mail.ru>
2021-04-29 17:55:04 +0300

    added logger for fixing errors

--- web-service/src/main/java/ann/example/webservice/controllers/AccountController.java
+++ web-service/src/main/java/ann/example/webservice/controllers/AccountController.java
@@ -3,11 +3,19 @@ package ann.example.webservice.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import ann.example.webservice.domain.Account;
 import ann.example.webservice.domain.Currency;
@@ -18,34 +26,56 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/account")
 public class AccountController {
 
+	 private final static Logger LOG = LoggerFactory.getLogger(AccountController.class);
+	
 	@Autowired
 	AccountService accountService;
 	
 	@GetMapping("/")
 	@ApiOperation(value = "Get all Accounts")
-	public List<Account> index(){
-		return accountService.getAccountsList();
+	public ResponseEntity<List<Account>> index(){
+		try {
+			return ResponseEntity.ok(accountService.getAccountsList());
+		} catch (DataAccessException e) {
+			LOG.error("Could not get accounts List!", e);
+			return ResponseEntity.status(500).build();
+		}
 	}
 	
 	@GetMapping("/{id}")
 	@ApiOperation(value = "Find Account by Id",
 				  notes = "Provide an id to look up specific Account from the DataBase")
-	public Account show(@PathVariable int id) {
-		return accountService.getAccountById(id);
+	public ResponseEntity<Account> show(@PathVariable int id) {
+		try {
+			return ResponseEntity.ok(accountService.getAccountById(id));
+		} catch (DataAccessException e) {
+			LOG.error("Could not get account by id!", e);
+			return ResponseEntity.status(500).build();
+		}		
 	}
-	
+
 	@GetMapping("/n/{number}")
 	@ApiOperation(value = "Find Account by Number",
 	  			  notes = "Provide a number to look up specific Account from the DataBase")
-	public Account show(@PathVariable String number) {
-		return accountService.getAccountByNumber(number);
+	public ResponseEntity<Account> show(@PathVariable String number) {
+		try {
+			return ResponseEntity.ok(accountService.getAccountByNumber(number));
+		} catch (DataAccessException e) {
+			LOG.error("Could not get account by number!", e);
+			return ResponseEntity.status(500).build();
+		}	
 	}
 	
 	@GetMapping("/c/{currency}")
 	@ApiOperation(value = "Find Accounts by Currency",
 	  			  notes = "Provide a currency to look up specific Account from the DataBase")
-	public List<Account> show(@PathVariable Currency currency) {
-		return accountService.getAccountByCurrency(currency);
+	public ResponseEntity<List<Account>> show(@PathVariable Currency currency) {
+		try {
+			return ResponseEntity.ok(accountService.getAccountByCurrency(currency));
+		} catch (DataAccessException e) {
+			LOG.error("Could not get accounts List by currency!", e);
+			return ResponseEntity.status(500).build();
+		}			
 	}
 	 
 	
--- web-service/src/main/java/ann/example/webservice/controllers/AdminController.java
+++ web-service/src/main/java/ann/example/webservice/controllers/AdminController.java
@@ -1,7 +1,11 @@
 package ann.example.webservice.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,31 +22,51 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/admin/account")
 public class AdminController {
 
+	private final static Logger LOG = LoggerFactory.getLogger(AdminController.class);
+	
 	@Autowired
 	AccountService accountService;
 	
 	@PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ApiOperation(value = "Create new Account",
 	  			  notes = "Provide a new Account to add specific Account in the DataBase")
-	public String create(@RequestBody Account account) {
-		accountService.saveAccount(account);
-		return "Account is created!";
+	public ResponseEntity<String> create(@RequestBody Account account) {
+		try{
+			accountService.saveAccount(account);
+			return new ResponseEntity<>("Account is saved", HttpStatus.OK);
+		} catch(RuntimeException e) {
+			String msg = "Cannot save the account!";
+			LOG.error(msg, e);
+			return new ResponseEntity<>(msg, HttpStatus.BAD_REQUEST);
+		}		
 	}
 	
 	@DeleteMapping("/{id}")
 	@ApiOperation(value = "Delete Account by Id",
 	  			  notes = "Provide an id to delete specific Account from the DataBase")
-	public String destroy(@PathVariable int id) {
-		accountService.deleteAccountById(id);
-		return "Account with id " + id + " is deleted!";
+	public ResponseEntity<String> destroy(@PathVariable int id) {
+		try{
+			accountService.deleteAccountById(id);
+			return new ResponseEntity<>("Account with id " + id + " is deleted!", HttpStatus.OK);
+		} catch(RuntimeException e) {
+			String msg = "Cannot delete the account!";
+			LOG.error(msg, e);
+			return new ResponseEntity<>(msg, HttpStatus.BAD_REQUEST);
+		}		
 	}
 	
 	@ApiOperation(value = "Update Account by Id",
 			      notes = "Provide an id and new properties to update specific Account from the DataBase")
 	@PutMapping("/{id}")
-	public String update(@PathVariable int id, @RequestBody Account newAccount) {
-		accountService.updateAccount(id, newAccount);		
-		return "Account with id " + id + " is updated!";
+	public ResponseEntity<String> update(@PathVariable int id, @RequestBody Account newAccount) {
+		try{
+			accountService.updateAccount(id, newAccount);
+			return new ResponseEntity<>("Account with id " + id + " is updated!", HttpStatus.OK);
+		} catch(RuntimeException e) {
+			String msg = "Cannot update the account!";
+			LOG.error(msg, e);
+			return new ResponseEntity<>(msg, HttpStatus.BAD_REQUEST);
+		}		
 	}
 	
 	
